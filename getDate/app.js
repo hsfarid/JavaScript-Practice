827a1e8d9d8c09172b0f1a349ccd061537950029
Farid Habib <hsfarid08@gmail.com>
2024-06-18 13:34:24 +0000

    modified app.js

--- getDate/app.js
+++ getDate/app.js
@@ -1,18 +1,24 @@
 //get html reference
-const resultScreen = document.querySelector(".resultScreen")
+const resultScreen = document.querySelector(".resultScreen");
 
 const getDate = () => {
-    const newDate = new Date();
-    const dd = newDate.getDate()
-    const mm = newDate.getMonth() + 1
-    const yyyy = newDate.getFullYear()
-    
-    const result = `${dd}/${mm}/${yyyy}`
-    let html = 
-    `
-    <p>Today's date is: ${result}</p>
-    `
-    resultScreen.innerHTML = html
-}
-getDate()
+  const newDate = new Date();
+  let dd = newDate.getDate();
+  let mm = newDate.getMonth() + 1;
+  const yyyy = newDate.getFullYear();
 
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+
+  const result = `${dd}/${mm}/${yyyy}`;
+
+  let html = `
+    <p>Today's date is: ${result}</p>
+    `;
+  resultScreen.innerHTML = html;
+};
+getDate();
