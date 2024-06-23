845481bea6696a90f304a46464407a72fda5a322
= <=>
2024-06-23 23:38:30 +0000

    array elements to string

---
+++ joinArrayElements/README.md
@@ -0,0 +1,1 @@
+JavaScript program to join all elements of an array into a string.
---
+++ joinArrayElements/app.js
@@ -0,0 +1,6 @@
+
+const names = ["Reed", "Ben", "Kwaku", "Amina"]
+
+console.log(names.toString())
+console.log(names.join())
+console.log(names.join("+"))
